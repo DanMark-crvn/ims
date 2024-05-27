bd4f736327e10edb6c14a529e0d1589551f7050f
Otnadnad <caravanadanmark@gmail.com>
2024-05-27 21:46:52 +0800

    first commit

---
+++ resources/views/department/create.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CREATE DEPARTMENT</title>
+</head>
+<body>
+    <h1>Create a Department</h1>
+    <form method="POST" action="">
+        <div>
+            <label>Department Name: </label>
+            <input type="text" name="deptName" placeholder="Enter a department" />
+        </div>
+        <div>
+            <input type="submit" value="Save" />
+        </div>
+    </form>
+</body>
+</html>
